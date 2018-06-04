6895364b43cea6d8573f327fae2e04a1ceea6718
Karrick S. McDermott <karrick@karrick.net>
2018-06-04 10:11:54 -0400

    updated example to use command line options

--- examples/using-int64/main.go
+++ examples/using-int64/main.go
@@ -7,31 +7,38 @@ import (
 	"time"
 
 	"github.com/karrick/gobptree"
+	"github.com/karrick/golf"
 )
 
 func main() {
-	const oneMillion = 1000000
-	const order = 64
+	count := golf.Int64("count", 1000000, "number of items")
+	order := golf.Int64("order", 8, "order of tree")
+	golf.Parse()
 
-	fmt.Printf("Creating a list of randomized integers from 0 to %d\n", oneMillion)
-	rand.Seed(time.Now().Unix())
-	randomizedValues := rand.Perm(oneMillion)
+	if *count <= 0 {
+		fmt.Fprintf(os.Stderr, "cannot run without size greater than 0: %d", *count)
+		os.Exit(2)
+	}
 
-	fmt.Printf("Creating a B+Tree of order %d, using int64 values as keys.\n", order)
-	t, err := gobptree.NewInt64Tree(order)
+	fmt.Printf("Creating a B+Tree of order %d, using int64 values as keys.\n", int(*order))
+	t, err := gobptree.NewInt64Tree(int(*order))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Insert the randomized values into the tree, creating a sorted list.\n")
+	fmt.Printf("Creating a list of randomized integers from 0 to %d\n", *count)
+	rand.Seed(time.Now().Unix())
+	randomizedValues := rand.Perm(int(*count))
+
+	fmt.Printf("Create a sorted list by inserting the randomized values into the tree.\n")
 	// For this example, we do not care about the value associated with each
 	// key.
 	for _, v := range randomizedValues {
 		t.Insert(int64(v), struct{}{})
 	}
 
-	fmt.Printf("Scan thru tree, collecting all keys in sorted order.\n")
+	fmt.Printf("Scan through tree, collecting all keys in sorted order.\n")
 	var sortedValues []int64
 	c := t.NewScanner(-1)
 	for c.Scan() {
@@ -43,7 +50,7 @@ func main() {
 	fmt.Printf("Searching tree for each value from the sorted list.\n")
 	// Ensure enumerated order of the keys are in fact sorted, in other words, a
 	// slice of int64 values from [0 to N).
-	for i := int64(0); i < oneMillion; i++ {
+	for i := int64(0); i < *count; i++ {
 		// Demonstrate searching for key, but disregard the returned value.
 		_, ok := t.Search(i)
 		if !ok {
