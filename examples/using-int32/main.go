96799f59e4ecbf59613d7b5360f94a3af53bd232
Karrick S. McDermott <karrick@karrick.net>
2018-11-28 20:52:44 +0000

    added int32 example

---
+++ examples/using-int32/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/karrick/gobptree"
+	"github.com/karrick/golf"
+)
+
+func main() {
+	count := golf.Int64("count", 1000000, "number of items")
+	order := golf.Int64("order", 8, "order of tree")
+	golf.Parse()
+
+	if *count <= 0 {
+		fmt.Fprintf(os.Stderr, "cannot run without size greater than 0: %d", *count)
+		os.Exit(2)
+	}
+
+	fmt.Printf("Creating a B+Tree of order %d, using int32 values as keys.\n", int(*order))
+	t, err := gobptree.NewInt32Tree(int(*order))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Creating a list of randomized integers from 0 to %d\n", *count)
+	rand.Seed(time.Now().Unix())
+	randomizedValues := rand.Perm(int(*count))
+
+	fmt.Printf("Create a sorted list by inserting the randomized values into the tree.\n")
+	// For this example, we do not care about the value associated with each
+	// key.
+	for _, v := range randomizedValues {
+		t.Insert(int32(v), struct{}{})
+	}
+
+	fmt.Printf("Scan through tree, collecting all keys in sorted order.\n")
+	var sortedValues []int32
+	c := t.NewScanner(0)
+	for c.Scan() {
+		// Get the key-value pair for this datum, but only collect the key.
+		k, _ := c.Pair()
+		sortedValues = append(sortedValues, k)
+	}
+
+	fmt.Printf("Searching tree for each value from the sorted list.\n")
+	// Ensure enumerated order of the keys are in fact sorted, in other words, a
+	// slice of int32 values from [0 to N).
+	for i := int32(0); i < int32(*count); i++ {
+		// Demonstrate searching for key, but disregard the returned value.
+		_, ok := t.Search(i)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "GOT: %v; WANT: %v", ok, true)
+			os.Exit(1)
+		}
+		// Ensure sortedValues[i] matches i.
+		if got, want := i, sortedValues[i]; got != want {
+			fmt.Fprintf(os.Stderr, "GOT: %v; WANT: %v", got, want)
+		}
+	}
+
+	fmt.Printf("Deleting all keys from the tree in randomized order.\n")
+	for _, v := range randomizedValues {
+		t.Delete(int32(v))
+	}
+}
